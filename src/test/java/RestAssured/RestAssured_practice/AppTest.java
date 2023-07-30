877d6bf9ed69ae435089bb708678d5408580786d
dell <dell@DESKTOP-BDE3M6O>
2023-07-30 12:07:55 +0530

    test

--- src/test/java/RestAssured/RestAssured_practice/AppTest.java
+++ src/test/java/RestAssured/RestAssured_practice/AppTest.java
@@ -16,9 +16,7 @@ public class AppTest
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
+   
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
